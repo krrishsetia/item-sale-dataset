b1410df606ca49a0bc8dfd975f41d578b868b84c
krrishsetia <88411219+krrishsetia@users.noreply.github.com>
2023-09-10 12:36:07 -0700

    Update session 33.py

--- session 33.py
+++ session 33.py
@@ -22,7 +22,7 @@ import multiprocessing
 pd.options.display.max_columns = 5
 pd.options.display.max_rows = 10000000
 
-data = pd.read_csv('csv files/Dataset.csv')
+data = pd.read_csv('Dataset.csv')
 temp1 = []
 temp2 = []
 temp3 = []
